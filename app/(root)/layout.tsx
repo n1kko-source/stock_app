df15474331faafcbf260ca04837843af142c80ba
n1kko-source <juannmunoz20@gmail.com>
2025-12-12 22:37:40 -0500

    Creación de Header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header"
+
+
+const layout = ({ children }: { children : React.ReactNode}) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+        {/* Header */}
+        <Header />
+        <div className="conainer py-10">
+            {children}
+        </div>
+    </main>
+  )
+}
+
+export default layout
